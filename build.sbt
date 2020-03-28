274ee975fd43b17c8ecc756fee3bb84e2b62106e
wwagner4 <wwagner4@gmail.com>
2020-03-28 12:46:12 +0100

    some refactoring before starting with the parser combinators

--- build.sbt
+++ build.sbt
@@ -4,3 +4,7 @@ organization := "entelijan"
 scalaVersion := "2.13.1"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.1" % "test"
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
+
+ideaPort in Global := 51932
+
--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -1,6 +1,7 @@
 import GainVal.GainVal
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
+import scala.util.parsing.combinator._
 
 object GainVal extends Enumeration {
   type GainVal = Value
@@ -12,59 +13,19 @@ case class Sound(pitch: Int,
                  gain: GainVal
                 )
 
+object MuplParser extends RegexParsers {
 
-class ParseSuite extends AnyFunSuite with Matchers {
-
-  val dataValidSounds = List(
-    ("(22|2|M)", Sound(22, 2, GainVal.M)),
-    ("(21|64|LL)", Sound(21, 64, GainVal.LL)),
-    ("(108|1|HH)", Sound(108, 1, GainVal.HH)),
-    ("(108|1|H)", Sound(108, 1, GainVal.H)),
-    ("(108|32|H)", Sound(108, 32, GainVal.H)),
-    ("(67|32|H)", Sound(67, 32, GainVal.H)),
-    ("(68|32|H )", Sound(68, 32, GainVal.H)),
-    ("(68|32|  H )", Sound(68, 32, GainVal.H)),
-    ("(68|32|)", Sound(68, 32, GainVal.M)),
-    ("(67||H)", Sound(67, 1, GainVal.H)),
-    ("(67| 2|H)", Sound(67, 2, GainVal.H)),
-    ("(67| 2  |H)", Sound(67, 2, GainVal.H)),
-    ("(|2|H)", Sound(44, 2, GainVal.H)),
-    ("(66 |2|H)", Sound(66, 2, GainVal.H)),
-    ("(  66 |2|H)", Sound(66, 2, GainVal.H)),
-  )
-
-  for ((in, should) <- dataValidSounds) {
-    test(s"parse Sound $in") {
-      parseSound(in).mustBe(should)
+  def parseSound1(input: String): Sound = {
+    val SoundRegex = """\((.*)\|(.*)\|(.*)\)""".r
+    input match {
+      case SoundRegex(a, b, c) => parseSound(a, b, c)
+      case _ => throw new IllegalArgumentException(s"$input does not confirm $SoundRegex")
     }
   }
-
-  val dataInvalidSounds = List(
-    ("(22|2|M1)", "Must be one of"),
-    ("(22|2|hallo)", "Must be one of"),
-    ("(22|2|123123)", "Must be one of"),
-    ("(22|2|LLL)", "Must be one of"),
-    ("(22|21|LL)", "Must be one of"),
-    ("(22|3|LL)", "Must be one of"),
-    ("(22|888|LL)", "Must be one of"),
-    ("(22|hallo|LL)", "Must be an integer"),
-    ("(hallo|1|LL)", "Must be an integer"),
-    ("(1|1|LL)", "Must be greater"),
-    ("(200|1|LL)", "Must be smaller"),
-  )
-
-  for ((in, should) <- dataInvalidSounds) {
-    test(s"parse invalid Sound $in") {
-      val thrown = intercept[Exception] {
-        parseSound(in)
-      }
-      val tm = thrown.getMessage
-      if (!tm.contains(should)) fail(s"Message '$tm' must contain '$should'")
-    }
-  }
-
-
-  def parseSound(input: String): Sound = {
+  
+  def parseSound(a: String, b: String, c: String): Sound = {
+    
+    val input = s"($a|$b|$c)"
 
     def toPitch(value: String): Int = {
       val valt = value.trim
@@ -108,11 +69,62 @@ class ParseSuite extends AnyFunSuite with Matchers {
       }
     }
 
-    val SoundRegex = """\((.*)\|(.*)\|(.*)\)""".r
-    input match {
-      case SoundRegex(a, b, c) => Sound(toPitch(a), toDuration(b), toGain(c))
-      case _ => throw new IllegalArgumentException(s"$input does not confirm $SoundRegex")
+    Sound(toPitch(a), toDuration(b), toGain(c))
+  }
+
+}
+
+
+class ParseSuite extends AnyFunSuite with Matchers {
+
+  val dataValidSounds = List(
+    ("(22|2|M)", Sound(22, 2, GainVal.M)),
+    ("(21|64|LL)", Sound(21, 64, GainVal.LL)),
+    ("(108|1|HH)", Sound(108, 1, GainVal.HH)),
+    ("(108|1|H)", Sound(108, 1, GainVal.H)),
+    ("(108|32|H)", Sound(108, 32, GainVal.H)),
+    ("(67|32|H)", Sound(67, 32, GainVal.H)),
+    ("(68|32|H )", Sound(68, 32, GainVal.H)),
+    ("(68|32|  H )", Sound(68, 32, GainVal.H)),
+    ("(68|32|)", Sound(68, 32, GainVal.M)),
+    ("(67||H)", Sound(67, 1, GainVal.H)),
+    ("(67| 2|H)", Sound(67, 2, GainVal.H)),
+    ("(67| 2  |H)", Sound(67, 2, GainVal.H)),
+    ("(|2|H)", Sound(44, 2, GainVal.H)),
+    ("(66 |2|H)", Sound(66, 2, GainVal.H)),
+    ("(  66 |2|H)", Sound(66, 2, GainVal.H)),
+  )
+
+  for ((in, should) <- dataValidSounds) {
+    test(s"parse Sound $in") {
+      MuplParser.parseSound1(in).mustBe(should)
     }
   }
+
+  val dataInvalidSounds = List(
+    ("(22|2|M1)", "Must be one of"),
+    ("(22|2|hallo)", "Must be one of"),
+    ("(22|2|123123)", "Must be one of"),
+    ("(22|2|LLL)", "Must be one of"),
+    ("(22|21|LL)", "Must be one of"),
+    ("(22|3|LL)", "Must be one of"),
+    ("(22|888|LL)", "Must be one of"),
+    ("(22|hallo|LL)", "Must be an integer"),
+    ("(hallo|1|LL)", "Must be an integer"),
+    ("(1|1|LL)", "Must be greater"),
+    ("(200|1|LL)", "Must be smaller"),
+  )
+
+  for ((in, should) <- dataInvalidSounds) {
+    test(s"parse invalid Sound $in") {
+      val thrown = intercept[Exception] {
+        MuplParser.parseSound1(in)
+      }
+      val tm = thrown.getMessage
+      if (!tm.contains(should)) fail(s"Message '$tm' must contain '$should'")
+    }
+  }
+
+
 
 }
