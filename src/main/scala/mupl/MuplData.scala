225986d5879a62402d8a8049c2593ef3bd37978d
wwagner4 <wwagner4@gmail.com>
2020-04-03 10:24:32 +0200

    moved data to separate file

---
+++ src/main/scala/mupl/MuplData.scala
@@ -0,0 +1,31 @@
+package mupl
+
+import mupl.GainVal.GainVal
+
+object GainVal extends Enumeration {
+  type GainVal = Value
+  val LL, L, M, H, HH = Value
+}
+
+sealed trait Chunk
+
+case class Melo(name: String,
+                sounds: List[Sound]
+               ) extends Chunk
+
+case class Variable(name: String,
+                    chunk: Chunk
+                   )
+
+case class Symbol(name: String) extends Chunk
+
+case class Sequence(chunks: List[Chunk]
+                   ) extends Chunk
+
+case class Parallel(sequences: List[Sequence]) extends Chunk
+
+case class Sound(pitch: Int,
+                 dur: Int,
+                 gain: GainVal
+                )
+
--- src/main/scala/mupl/MuplParser.scala
+++ src/main/scala/mupl/MuplParser.scala
@@ -1,35 +1,8 @@
 package mupl
 
-import mupl.GainVal.GainVal
-
 import scala.util.parsing.combinator.RegexParsers
 
-object GainVal extends Enumeration {
-  type GainVal = Value
-  val LL, L, M, H, HH = Value
-}
-
-sealed trait Chunk
-
-case class Melo(name: String,
-                sounds: List[Sound]
-               ) extends Chunk
-
-case class Variable(name: String,
-                    chunk: Chunk
-                   )
-
-case class Symbol(name: String) extends Chunk
-
-case class Sequence(chunks: List[Chunk]
-                   ) extends Chunk
-
-case class Parallel(sequences: List[Sequence]) extends Chunk
-
-case class Sound(pitch: Int,
-                 dur: Int,
-                 gain: GainVal
-                )
+import mupl.GainVal.GainVal
 
 object MuplParser extends RegexParsers {
 
