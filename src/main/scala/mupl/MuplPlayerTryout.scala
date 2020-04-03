73137e0bbfac42712a4df4b5b2e32a68c678a89b
wwagner4 <wwagner4@gmail.com>
2020-04-03 19:18:58 +0200

    minor fixes

--- src/main/scala/mupl/MuplPlayerTryout.scala
+++ src/main/scala/mupl/MuplPlayerTryout.scala
@@ -7,14 +7,16 @@ import org.slf4j.LoggerFactory
 object MuplPlayerTryout extends App {
 
   val logger = LoggerFactory.getLogger("tryout")
-  
+
   try {
     val base = Path.of("src", "main", "chuck", "base.ck")
     val play = Path.of("src", "main", "chuck", "play.ck")
+    logger.info(s"Starting ${base.toString} ${play.toString}")
     val p = new MuplPlayer
     val res = p.play(base, play)
     res match {
-      case Some(msg) => logger.info(msg) 
+      case Some(msg) => logger.info(msg)
+      case None => logger.info("Finished successfull !!")
     }
   } catch {
     case e: Exception => logger.error(e.getMessage)
