131e50f60b83f01911a512b20e18509c3d9ea359
wwagner4 <wwagner4@gmail.com>
2020-04-05 17:49:27 +0200

    some css

--- src/main/scala/mupl/MuplServlet.scala
+++ src/main/scala/mupl/MuplServlet.scala
@@ -1,7 +1,6 @@
 package mupl
 
 import java.nio.file.{Files, Path, Paths}
-import java.util.concurrent.TimeUnit
 
 import org.scalatra._
 import org.slf4j.LoggerFactory
@@ -10,7 +9,6 @@ import scala.concurrent.Future
 import scala.jdk.CollectionConverters._
 import scala.concurrent._
 import ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
 
 class MuplServlet extends ScalatraServlet {
 
@@ -23,29 +21,29 @@ class MuplServlet extends ScalatraServlet {
 
   get("/") {
     contentType = "text/html"
-    bodyCreate(None)
+    htmlCreate(bodyCreate(None))
   }
 
-
   get("/action/:file") {
-    contentType = "text/html"
+    contentType = "text/html" 
     val file = params("file")
     logger.info(s"Clicked on $file")
     _selectedMuplFile = Some(file)
-    bodyCreate(None)
+    htmlCreate(bodyCreate(None))
   }
 
   get("/stop") {
     contentType = "text/html"
     _player.stop()
-    bodyCreate(None)
+    htmlCreate(bodyCreate(None))
   }
 
   post("/play") {
     contentType = "text/html"
     try {
       _selectedMuplFile match {
-        case None => bodyCreate(Some("No mupl file selected"))
+        case None =>
+          htmlCreate(bodyCreate(Some("No mupl file selected")))
         case Some(mf) =>
           logger.info("Clicked the play button")
           val mupl: String = params("mupl")
@@ -56,30 +54,55 @@ class MuplServlet extends ScalatraServlet {
           }
           Thread.sleep(1000)
           playResult match {
-            case None => bodyCreatePlaying()
+            case None => 
+              htmlCreate(bodyCreatePlaying())
             case Some(pr) => pr match {
-              case None => bodyCreate(None)
+              case None => 
+                htmlCreate(bodyCreate(None))
               case Some(msg) =>
                 val m = s"Error in player:\n$msg"
                 logger.info(m)
-                bodyCreate(Some(m))
+                htmlCreate(bodyCreate(Some(m)))
             }  
           }
       }
     } catch {
       case e: Exception =>
         logger.error(s"Error playing. ${e.getMessage}", e)
-        bodyCreate(Some(s"Error playing. ${e.getMessage}"))
+        htmlCreate(bodyCreate(Some(s"Error playing. ${e.getMessage}")))
     }
   }
 
-  def bodyCreate(msg: Option[String]): String =
+  def htmlCreate(body: String): String = {
     s"""
        |<html>
        |<head>
        |    <title>mupl player</title>
+       |    <style type="text/css">
+       |        * { 
+       |            font-family : "Lucida Console", Monospace;
+       |            font-size : 15px;
+       |            outline: none;}
+       |        body { 
+       |            margin : 20px 10px 20px 10px;}
+       |        textarea {
+       |            width: 100%;
+       |            height: 50%; 
+       |            box-sizing: border-box;
+       |            background-color: azure;
+       |            border: none;
+       |            padding: 5px;}
+       |    </style>
        |</head>
        |<body>
+       |$body
+       |</body
+       |</html>
+       |""".stripMargin
+  } 
+  
+  def bodyCreate(msg: Option[String]): String =
+    s"""
        |    <table>
        |        <tr>
        |            $tdMuplFiles
@@ -87,7 +110,7 @@ class MuplServlet extends ScalatraServlet {
        |</table>
        |$pSelectedMuplFile
        |<form action="/play" method="post">
-       |<textarea id="mupl" name="mupl" rows="20" cols="50">
+       |<textarea id="mupl" name="mupl" class="nosel">
        |$txtMupl
        |</textarea>
        |<p>
@@ -95,24 +118,15 @@ class MuplServlet extends ScalatraServlet {
        |</p>
        |</form>
        |${pMessage(msg)}
-       |</body
-       |</html>
        |""".stripMargin
 
   def bodyCreatePlaying(): String =
     s"""
-       |<html>
-       |<head>
-       |    <title>mupl player</title>
-       |</head>
-       |<body>
        |$pSelectedMuplFile
        |<p>
        |${htmlFormat(txtMupl)}
        |</p>
        |<a href="/stop">stop running</a>
-       |</body>
-       |</html>
        |""".stripMargin
 
   def pMessage(msg: Option[String]): String = {
@@ -128,10 +142,23 @@ class MuplServlet extends ScalatraServlet {
   def htmlFormat(str: String): String = {
     str
       .split("\n")
+      .map(escapeBlanks)
       .map(l => s"$l<br/>")
       .mkString("\n")
   }
 
+  def escapeBlanks(in: String): String = {
+    in.toList
+      .map(_.toString)
+      .map(escapeBlank)
+      .mkString("")
+  }
+  
+  def escapeBlank(s: String): String = {
+    if (s == " ") """&nbsp;"""
+    else s
+  }
+  
   def pSelectedMuplFile: String = {
     _selectedMuplFile match {
       case None => """<p>No mupl file selected</p>"""
